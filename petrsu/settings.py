3f3bfd84f63ebaf806df614040bcd8e1530f19de
Yamushev Igor <yamushev.igro@yandex.ru>
2018-03-17 16:05:13 +0300

    Add allowed hosts

--- petrsu/settings.py
+++ petrsu/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', '127.0.0.1:8000', 'localhost', 'localhost:8000']
+ALLOWED_HOSTS = ['127.0.0.1', '127.0.0.1:8000', 'localhost', 'localhost:8000', 'petrsu-doors.herokuapp.com']
 
 
 # Application definition
